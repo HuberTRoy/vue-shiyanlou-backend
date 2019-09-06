ff527ac5c397e2134f304d7ceef1b3511a9c3230
cyrbuzz <cyrbuzz@foxmail.com>
2019-09-06 17:21:12 +0800

    vue-shiyanlou backend for proxy

---
+++ api/urls.py
@@ -0,0 +1,33 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path('v2/courses/', views.courses, name='courses'),
+    path('v2/courses/<int:courseId>/', views.course, name='course'),
+    path('v2/courses/<int:courseId>/labs/', views.courseLabs, name='courseLabs'),
+    path('v2/courses/categories/', views.categories, name='categories'),
+
+    path('v2/index/categories/', views.indexCategories, name='indexCategories'),
+    path('v2/index/banner-pictures/', views.indexBanner, name='indexBanner'),
+    path('v2/index/louplus/', views.louplus, name='louplus'),
+    path('v2/index/classfication-courses/', views.classficationCourses, name='classficationCourses'),
+    path('v2/index/bootcamps/', views.indexBootcamps, name='indexBootcamps'),
+    path('v2/index/paths/', views.indexPaths, name='indexPath'),
+
+    path('v2/questions/', views.questions, name='questions'),
+    path('v2/fringe/recent-activities/', views.recentActivities, name='recentActivities'),
+    path('v2/fringe/recent-louplus-courses/', views.recentLouplus, name='recentLouplus'),    
+    path('v2/questions/<int:questionId>/', views.question, name='question'),
+    path('v2/questions/<int:questionId>/answers/', views.questionAnswers, name='questionAnswers'),
+    path('v2/questions/<int:questionId>/related-questions/', views.relatedQuestions, name='relatedQuestions'),
+
+    path('v2/paths', views.paths, name='paths'),
+    path('v2/paths/<int:pathId>/', views.path, name='path'),
+    path('v2/paths/<int:pathId>/stages/', views.stages, name='pathStages'),
+
+    path('v2/comments/', views.comment, name='comments'),
+
+    path('v2/labreports', views.labreports, name='labreports')
+]
+
---
+++ api/views.py
@@ -0,0 +1,99 @@
+from django.http import HttpResponse, JsonResponse
+import requests
+
+# def makeGET(params):
+#     """
+#         {params:1, params2: 2} to params=1&params2=2
+#     """
+
+baseUrl = "https://www.shiyanlou.com/api/v2/"
+
+#comment
+def comment(request):
+    content = requests.get(f"{baseUrl}comments/", params=request.GET)
+    return JsonResponse(content.json(), safe=False)
+
+# path
+def stages(request, pathId):
+    content = requests.get(f"{baseUrl}paths/{pathId}/stages")
+    return JsonResponse(content.json(), safe=False)
+
+def path(request, pathId):
+    content = requests.get(f"{baseUrl}paths/{pathId}")
+    return JsonResponse(content.json(), safe=False)
+
+def paths(request):
+    content = requests.get(f"{baseUrl}paths")
+    return JsonResponse(content.json(), safe=False)
+
+# qa
+def relatedQuestions(request, questionId):
+    content = requests.get(f"{baseUrl}questions/{questionId}/related-questions/")
+    return JsonResponse(content.json(), safe=False)
+
+def question(request, questionId):
+    content = requests.get(f"{baseUrl}questions/{questionId}")
+    return JsonResponse(content.json(), safe=False)
+
+def questionAnswers(request, questionId):
+    content = requests.get(f"{baseUrl}questions/{questionId}/answers/", params=request.GET)
+    return JsonResponse(content.json(), safe=False)
+
+def recentLouplus(request):
+    content = requests.get(f"{baseUrl}fringe/recent-louplus-courses/")
+    return JsonResponse(content.json(), safe=False)
+
+def recentActivities(request):
+    content = requests.get(f"{baseUrl}fringe/recent-activities/")
+    return JsonResponse(content.json(), safe=False)
+
+def questions(request):
+    content = requests.get(f"{baseUrl}questions/", params=request.GET)
+    return JsonResponse(content.json(), safe=False)
+
+
+# home
+def indexPaths(request):
+    content = requests.get(f"{baseUrl}index/paths")
+    return JsonResponse(content.json(), safe=False)
+
+def indexBootcamps(request):
+    content = requests.get(f"{baseUrl}index/bootcamps")
+    return JsonResponse(content.json(), safe=False)
+
+def classficationCourses(request):
+    content = requests.get(f"{baseUrl}index/classfication-courses/")
+    return JsonResponse(content.json(), safe=False)
+
+def louplus(request):
+    content = requests.get(f"{baseUrl}index/louplus/")
+    return JsonResponse(content.json(), safe=False)
+
+def indexBanner(request):
+    content = requests.get(f"{baseUrl}index/banner-pictures/")
+    return JsonResponse(content.json(), safe=False)
+
+def indexCategories(request):
+    content = requests.get("http://www.shiyanlou.com/api/v2/index/categories/")
+    return JsonResponse(content.json(), safe=False)
+
+# courses 
+def courseLabs(request, courseId):
+    content = requests.get(f"{baseUrl}courses/{courseId}/labs")
+    return JsonResponse(content.json(), safe=False)
+
+def course(request, courseId):
+    content = requests.get(f"{baseUrl}courses/{courseId}")
+    return JsonResponse(content.json(), safe=False)
+
+def categories(request):
+    content = requests.get("http://www.shiyanlou.com/api/v2/courses/categories/")
+    return JsonResponse(content.json(), safe=False)
+
+def courses(request):
+    content = requests.get("http://www.shiyanlou.com/api/v2/courses/", params=request.GET)
+    return JsonResponse(content.json(), safe=False)
+
+def labreports(request):
+    content = requests.get(f"{baseUrl}labreports/", params=request.GET)
+    return JsonResponse(content.json(), safe=False)
